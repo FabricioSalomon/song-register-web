834158e919ba1e6e34726a654352253dd40cb106
Fabricio Salomon <fabriciosalomon14@gmail.com>
2024-04-21 16:26:06 -0300

    feat: add hooks, entities and query keys

---
+++ src/utils/query-keys/index.ts
@@ -0,0 +1,11 @@
+import { GET_AUTHORS_LIST } from "./authors";
+import { GET_SONGS_LIST } from "./songs";
+
+export const QUERY_KEYS = {
+  AUTHORS: {
+    GET_AUTHORS_LIST,
+  },
+  SONGS: {
+    GET_SONGS_LIST,
+  },
+};
