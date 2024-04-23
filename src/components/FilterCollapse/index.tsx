129d9100d5947d8062e181f7d8b2466986efb0f6
Fabrício Salomon <fabriciosalomon14@gmail.com>
2024-04-23 13:58:59 -0300

    fix: filter collapse

--- src/components/FilterCollapse/index.tsx
+++ src/components/FilterCollapse/index.tsx
@@ -6,16 +6,21 @@ import { CollapseContainer } from "./styles";
 const { Panel } = Collapse;
 
 interface FilterCollapseProps {
+  disabled?: boolean;
   children: React.ReactNode;
   hasSearchParams?: boolean;
 }
 
 export function FilterCollapse({
   children,
+  disabled = false,
   hasSearchParams = true,
 }: Readonly<FilterCollapseProps>) {
   return (
-    <CollapseContainer defaultActiveKey={hasSearchParams ? "1" : "0"}>
+    <CollapseContainer
+      collapsible={disabled ? "disabled" : "header"}
+      defaultActiveKey={hasSearchParams ? "1" : "0"}
+    >
       <Panel key="1" header="Filter">
         {children}
       </Panel>
--- src/components/FilterCollapse/styles.ts
+++ src/components/FilterCollapse/styles.ts
@@ -4,21 +4,45 @@ import styled from "styled-components";
 export const CollapseContainer = styled(Collapse)`
   border-radius: 0.5rem;
 
-  .ant-collapse-item {
-    border-radius: 0.5rem;
-    border: none;
+  .ant-collapse-item-disabled {
+    background-color: #b3b3b3;
 
     .ant-collapse-header {
       border-radius: 0.5rem;
-	  color: ${({ theme }) => theme.fontColor};
+    }
+  }
+
+  .ant-collapse-item {
+    border: none;
+    border-radius: 0.5rem;
+
+    .ant-collapse-header {
+      padding: 0 16px;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      border-radius: 0.5rem;
+      color: ${({ theme }) => theme.fontColor};
 
       .ant-collapse-header-text {
         width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: row;
       }
     }
 
     .ant-collapse-content-active {
-      border-radius: 0 0 0.5rem 0.5rem;
+      border-radius: 0.5rem;
+    }
+
+    &:last-child {
+      border-radius: 0.5rem;
+      .ant-collapse-header {
+        border-radius: 0.5rem;
+      }
     }
   }
 
